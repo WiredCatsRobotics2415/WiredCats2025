43cfb7e906eda9d410604dc78b18a26e469f76f0
Snapwhiz914 <asilversind@gmail.com>
2025-01-06 17:09:13 -0500

    Basic files and organization added

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,16 @@
+package main.java.frc.robot;
+
+public class RobotContainer {
+    private static RobotContainer instance;
+
+    private RobotContainer() {
+        
+    }
+
+    public static RobotContainer getInstance() {
+        if (instance == null) {
+            instance = new RobotContainer();
+        }
+        return instance;
+    }
+}
