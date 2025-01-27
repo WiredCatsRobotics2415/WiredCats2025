bd82b3b5cbed48d1cceadb9f06a4f17c467cd011
108charlotte <108sarahsmith@gmail.com>
2025-01-27 17:55:58 -0500

    Added ElevatorIOSim.java and ElevatorIOReal.java, nothing in there yet though

--- src/main/java/frc/subsystems/elevator/ElevatorIO.java
+++ src/main/java/frc/subsystems/elevator/ElevatorIO.java
@@ -17,6 +17,6 @@ public class ElevatorIO {
     }
 
     public void setVoltage(double volts) {
-
+        
     }
 }
---
+++ src/main/java/frc/subsystems/elevator/ElevatorIOReal.java
@@ -0,0 +1,5 @@
+package frc.subsystems.elevator;
+
+public class ElevatorIOReal {
+    
+}
