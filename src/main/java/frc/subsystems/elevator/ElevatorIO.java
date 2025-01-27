475023673a5e7c566cf2846e589a3911c316db38
108charlotte <108sarahsmith@gmail.com>
2025-01-27 17:53:24 -0500

    added skeleton code for ElevatorIO.java

---
+++ src/main/java/frc/subsystems/elevator/ElevatorIO.java
@@ -0,0 +1,22 @@
+package frc.subsystems.elevator;
+
+public class ElevatorIO {
+    
+
+    public class ElevatorIOInputs {
+        public double wirePotentiometerValue; 
+        
+        
+        public boolean isConnectedLeft; 
+        public double temperatureLeft; 
+        public double currentDrawLeft; 
+
+        public boolean isConnectedRight; 
+        public double temperatureRight; 
+        public double currentDrawRight; 
+    }
+
+    public void setVoltage(double volts) {
+
+    }
+}
