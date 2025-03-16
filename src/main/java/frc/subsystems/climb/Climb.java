3bf8827a262a03b952ef60ae1986de87a8fd62e1
Liam Loughead (Code Field Laptop) <64972906+Snapwhiz914@users.noreply.github.com>
2025-03-16 08:58:56 -0400

    push

--- src/main/java/frc/subsystems/climb/Climb.java
+++ src/main/java/frc/subsystems/climb/Climb.java
@@ -1,16 +1,24 @@
 package frc.subsystems.climb;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.utils.Util;
 
 public class Climb extends SubsystemBase {
     private static Climb instance;
+
+    private ClimbIO io;
+
+    private Climb() {
+        io = (ClimbIO) Util.getIOImplementation(ClimbIOReal.class, ClimbIO.class, new ClimbIO() {});
+    }
 
     public static Climb getInstance() {
         if (instance == null) instance = new Climb();
         return instance;
     }
 
-    private Climb() {
-
+    public Command runForward() {
+        return 
     }
 }
