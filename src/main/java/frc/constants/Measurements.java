eb812b256e95c16f5b60b97fd55899d46eb430c7
Celeste Roselli <72556836+celesteroselli@users.noreply.github.com>
2025-03-20 13:21:22 -0400

    HPS values

--- src/main/java/frc/constants/Measurements.java
+++ src/main/java/frc/constants/Measurements.java
@@ -163,10 +163,14 @@ public class Measurements {
     }
 
     public class HumanPlayerStation {
-        public static final Pose2d blueLeftHPS = new Pose2d(new Translation2d(0.8613139999999999, 7.414259999999999), Rotation2d.fromDegrees(-36));
-        public static final Pose2d blueRightHPS = new Pose2d(new Translation2d(0.8613139999999999, 0.628142), Rotation2d.fromDegrees(36));
-        public static final Pose2d redLeftHPS = new Pose2d(new Translation2d(16.687292, 0.628142), Rotation2d.fromDegrees(-36));
-        public static final Pose2d redRightHPS = new Pose2d(new Translation2d(16.687292, 7.414259999999999), Rotation2d.fromDegrees(36));
+        public static final Pose2d blueLeftHPS = new Pose2d(new Translation2d(0.8613139999999999, 7.414259999999999),
+            Rotation2d.fromDegrees(-36));
+        public static final Pose2d blueRightHPS = new Pose2d(new Translation2d(0.8613139999999999, 0.628142),
+            Rotation2d.fromDegrees(36));
+        public static final Pose2d redLeftHPS = new Pose2d(new Translation2d(16.687292, 0.628142),
+            Rotation2d.fromDegrees(-36));
+        public static final Pose2d redRightHPS = new Pose2d(new Translation2d(16.687292, 7.414259999999999),
+            Rotation2d.fromDegrees(36));
 
         public static final int blueLeftId = 13;
         public static final int blueRightId = 12;
